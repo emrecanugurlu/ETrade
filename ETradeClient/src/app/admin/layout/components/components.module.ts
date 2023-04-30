ace35f882f87c34a4ca79e5f1983d3d36043b93a
Emre Can <emrecnugurlu@gmail.com>
2023-04-30 17:30:10 +0300

    Angularda routing yapılanması oluşturuldu.

--- ETradeClient/src/app/admin/layout/components/components.module.ts
+++ ETradeClient/src/app/admin/layout/components/components.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SlidebarComponent } from './slidebar/slidebar.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -13,7 +14,8 @@ import { SlidebarComponent } from './slidebar/slidebar.component';
     SlidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports:[
     HeaderComponent,
--- ETradeClient/src/app/app-routing.module.ts
+++ ETradeClient/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { HomeComponent } from './ui/components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"basket", loadChildren:() => import("./ui/components/basket/basket.module").then(m=>m.BasketModule)},
+  {path:"product", loadChildren:() => import("./ui/components/product/product.module").then(m=>m.ProductModule)},
+  {
+    path: "admin", component: LayoutComponent, children: [
+      { path: "", loadChildren: () => import("./admin/components/dashboard/dashboard.module").then(m => m.DashboardModule) },
+      { path: "customer", loadChildren: () => import("./admin/components/customer/customer.module").then(m => m.CustomerModule) },
+      { path: "order", loadChildren: () => import("./admin/components/order/order.module").then(m => m.OrderModule) },
+      { path: "product", loadChildren: () => import("./admin/components/product/product.module").then(m => m.ProductModule) },
+
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
